3374ea59f3e8f56ea173eb8edb4361a400954cf5
caninanir <can.inanir@sabanciuniv.edu>
2024-08-02 15:22:39 +0300

    SPACEX-42 CLEANUP - Separated Detail and Card items

--- app/src/main/java/com/can_inanir/spacex/ui/feature/informationscreens/LaunchCard.kt
+++ app/src/main/java/com/can_inanir/spacex/ui/feature/informationscreens/LaunchCard.kt
@@ -4,18 +4,13 @@ import android.content.Intent
 import android.net.Uri
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -26,10 +21,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -43,57 +36,6 @@ import com.can_inanir.spacex.data.local.entities.RocketEntity
 import com.can_inanir.spacex.data.remote.LaunchpadDetailViewModel
 import com.can_inanir.spacex.data.remote.RocketDetailViewModel
 import com.can_inanir.spacex.ui.main.AppColors
-
-@Composable
-fun RocketCard(
-    rocket: RocketEntity,
-    isFavorite: Boolean,
-    onClick: () -> Unit,
-    onFavoriteClick: () -> Unit
-) {
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .clickable(onClick = onClick)
-            .padding(horizontal = 16.dp),
-        colors = CardDefaults.cardColors(containerColor = AppColors.CardBackground)
-    ) {
-        Column {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Text(
-                    modifier = Modifier.padding(16.dp),
-                    text = rocket.name,
-                    style = MaterialTheme.typography.headlineLarge,
-                    color = AppColors.White,
-                    fontFamily = FontFamily(Font(R.font.nasalization, FontWeight.Normal))
-                )
-                IconButton(onClick = onFavoriteClick, modifier = Modifier.size(64.dp)) {
-                    Icon(
-                        modifier = Modifier.size(64.dp),
-                        tint = Color.Unspecified,
-                        painter = painterResource(
-                            id = if (isFavorite) R.drawable.buttons_icons_favorites_active_pressed else R.drawable.buttons_icons_favorites_enable
-                        ),
-                        contentDescription = null
-                    )
-                }
-            }
-            rocket.flickrImages.firstOrNull()?.let { imageUrl ->
-                Image(
-                    painter = rememberAsyncImagePainter(model = imageUrl),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(300.dp),
-                    contentScale = ContentScale.FillBounds
-                )
-            }
-        }
-    }
-}
 
 @Composable
 fun LaunchCard(
--- app/src/main/java/com/can_inanir/spacex/ui/feature/informationscreens/LaunchDetail.kt
+++ app/src/main/java/com/can_inanir/spacex/ui/feature/informationscreens/LaunchDetail.kt
@@ -34,15 +34,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
 import com.can_inanir.spacex.R
 import com.can_inanir.spacex.data.local.entities.LaunchEntity
@@ -55,133 +52,6 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 import java.util.TimeZone
-
-@Composable
-fun RocketDetail(
-    rocket: RocketEntity,
-    isFavorite: Boolean,
-    onClose: () -> Unit,
-    onFavoriteClick: (String) -> Unit
-) {
-    val context = LocalContext.current
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .verticalScroll(rememberScrollState())
-            .background(AppColors.HalfGrayTransparentBackground)
-    ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 50.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            IconButton(onClick = onClose, modifier = Modifier.size(48.dp)) {
-                Icon(
-                    imageVector = Icons.AutoMirrored.Filled.KeyboardArrowLeft,
-                    contentDescription = null,
-                    tint = AppColors.White,
-                    modifier = Modifier.size(48.dp)
-                )
-            }
-            Spacer(modifier = Modifier.weight(1f))
-            Text(
-                text = rocket.name,
-                style = MaterialTheme.typography.headlineLarge,
-                color = AppColors.CoolGreen,
-                fontFamily = FontFamily(Font(R.font.nasalization, FontWeight.Normal)),
-                modifier = Modifier.align(Alignment.CenterVertically),
-                softWrap = false
-            )
-            Spacer(modifier = Modifier.weight(1f))
-            IconButton(onClick = { onFavoriteClick(rocket.name) }) {
-                Icon(
-                    painter = painterResource(
-                        id = if (isFavorite) R.drawable.buttons_icons_favorites_active_pressed else R.drawable.buttons_icons_favorites_enable
-                    ),
-                    contentDescription = null,
-                    tint = Color.Unspecified
-                )
-            }
-        }
-        rocket.flickrImages.firstOrNull()?.let { imageUrl ->
-            Image(
-                painter = rememberAsyncImagePainter(model = imageUrl),
-                contentDescription = null,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp)
-                    .heightIn(max = 300.dp),
-                contentScale = ContentScale.Crop
-            )
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(
-            text = rocket.description,
-            style = MaterialTheme.typography.bodyLarge,
-            color = AppColors.White,
-            modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        DetailItem(label = "Height", value = "${rocket.height.meters}m / ${rocket.height.feet} ft")
-        HorizontalDivider(color = AppColors.DividerColor, thickness = 1.dp)
-        DetailItem(
-            label = "Diameter",
-            value = "${rocket.diameter.meters}m / ${rocket.diameter.feet} ft"
-        )
-        HorizontalDivider(color = AppColors.DividerColor, thickness = 1.dp)
-        DetailItem(label = "Mass", value = "${rocket.mass.kg} kg / ${rocket.mass.lb} lb")
-        HorizontalDivider(color = AppColors.DividerColor, thickness = 1.dp)
-        rocket.payloadWeights.find { it.id == "leo" }?.let {
-            DetailItem(
-                label = "Payload to LEO",
-                value = "${it.kg} kg / ${it.lb} lb"
-            )
-            HorizontalDivider(color = AppColors.DividerColor, thickness = 1.dp)
-        }
-        rocket.payloadWeights.find { it.id == "gto" }?.let {
-            DetailItem(
-                label = "Payload to GTO",
-                value = "${it.kg} kg / ${it.lb} lb"
-            )
-            HorizontalDivider(color = AppColors.DividerColor, thickness = 1.dp)
-        }
-        rocket.payloadWeights.find { it.id == "mars" }?.let {
-            DetailItem(
-                label = "Payload to Mars",
-                value = "${it.kg} kg / ${it.lb} lb"
-            )
-            HorizontalDivider(color = AppColors.DividerColor, thickness = 1.dp)
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(
-            colors = ButtonDefaults.buttonColors(
-                containerColor = AppColors.CoolGreen,
-                contentColor = AppColors.White
-            ),
-            onClick = {
-                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(rocket.wikipedia))
-                context.startActivity(intent)
-            },
-            modifier = Modifier.align(Alignment.CenterHorizontally)
-        ) {
-            Text("Learn More")
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-        rocket.flickrImages.drop(1).forEach { imageUrl ->
-            Image(
-                painter = rememberAsyncImagePainter(model = imageUrl),
-                contentDescription = null,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp)
-                    .heightIn(max = 300.dp),
-                contentScale = ContentScale.Crop
-            )
-        }
-    }
-}
 
 @Composable
 fun LaunchDetail(
@@ -301,32 +171,6 @@ fun LaunchDetail(
             )
         }
         Spacer(modifier = Modifier.height(16.dp))
-    }
-}
-
-@Composable
-fun DetailItem(label: String, value: String) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 16.dp, vertical = 8.dp),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween
-    ) {
-        Text(
-            text = label,
-            style = MaterialTheme.typography.labelLarge.copy(
-                fontWeight = FontWeight.Bold,
-                fontSize = 14.sp
-            ),
-            color = AppColors.White
-        )
-        Spacer(modifier = Modifier.weight(1f))
-        Text(
-            text = value,
-            style = MaterialTheme.typography.bodyLarge,
-            color = AppColors.White
-        )
     }
 }
 
