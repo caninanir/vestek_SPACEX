f383b89c4b8b0462a963973c5580d6d19ec769b5
caninanir <can.inanir@sabanciuniv.edu>
2024-08-02 09:44:26 +0300

    SpaceX-37: CLEANUP - Updated MainActivity Separated the logic inside MainActivity to multiple classes.

--- app/src/main/java/com/can_inanir/spacex/ui/main/MainActivity.kt
+++ app/src/main/java/com/can_inanir/spacex/ui/main/MainActivity.kt
@@ -5,42 +5,29 @@ import android.view.Window
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.view.WindowCompat
-import com.can_inanir.spacex.ui.feature.login.AuthViewModel
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.common.api.ApiException
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    @Inject lateinit var googleSignInClient: GoogleSignInClient
-    lateinit var authViewModel: AuthViewModel
-
-    private val googleSignInLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-        val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
-        val account: GoogleSignInAccount? = task.getResult(ApiException::class.java)
-        account?.idToken?.let {
-            authViewModel.handleGoogleAccessToken(it)
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Remove title bar
         requestWindowFeature(Window.FEATURE_NO_TITLE)
-        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+
+        // Enable full-screen mode
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+
+        // Ensure system windows fit properly
         WindowCompat.setDecorFitsSystemWindows(window, true)
 
         setContent {
-            MyApp { signInWithGoogle() }
+            SpaceXMainApp()
         }
-    }
-
-    private fun signInWithGoogle() {
-        val signInIntent = googleSignInClient.signInIntent
-        googleSignInLauncher.launch(signInIntent)
     }
 }
